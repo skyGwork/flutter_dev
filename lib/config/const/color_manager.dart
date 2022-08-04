567db2ad57daf202b3525eb4bf04490c4d427ee4
skyGwork <skyGworkk@gmail.com>
2022-08-04 12:12:29 +0530

    update

--- lib/config/const/color_manager.dart
+++ lib/config/const/color_manager.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class ColorManager {
   static Color primary = const Color(0xFF607D8B);
   static Color white = const Color(0xFFFFFFFF);
-  static Color dark = const Color(0xFF1A181D);
+  static Color dark = const Color(0xFF454545);
 }
